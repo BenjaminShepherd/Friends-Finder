7ec28e107c6a7bd55a2aac4c9c8bfad15a547111
BenShep <19ute14@gmail.com>
2019-05-07 18:27:20 -0600

    working server

---
+++ server.js
@@ -0,0 +1,21 @@
+
+
+// Dependencies
+const express = require("express");
+
+// Express Configuration
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("app/public"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routing
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
